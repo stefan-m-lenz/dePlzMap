b9fc62d94108709ac35202c4188269f67cc8b823
Stefan Lenz <stefan.lenz@uni-mainz.de>
2023-03-08 16:07:56 +0100

    Argument "populationRelative"

--- R/dePlzHeatmap.R
+++ R/dePlzHeatmap.R
@@ -1,4 +1,3 @@
-library(ggplot2)
 
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage("Preparing map data (this might take some time)")
@@ -18,13 +17,21 @@ library(ggplot2)
 #'
 #' @return a plot of Germany or a set of countries in Germany with colored PLZ areas
 dePlzHeatmap <- function(data, title = "", bundesland = NA,
-                         populationRelative = FALSE, color = "#115e01") {
+                         populationRelative = NA, color = "#115e01") {
 
   if (!is.data.frame(data) || ncol(data) != 2 || !("plz" %in% colnames(data))) {
     stop("Argument \"data\" must be a two-column data frame with one column named \"plz\".")
   }
 
   otherColName <- colnames(data)[colnames(data) != "plz"]
+
+  if (!is.na(populationRelative)) {
+    data <- merge(data, populationData[, c("plz", "population")], by = "plz")
+    newOtherColName <- paste(otherColName, populationRelative)
+    data[, newOtherColName] <- data[, otherColName] / data$population
+    data[, otherColName] <- NULL
+    otherColName <- newOtherColName
+  }
 
   if (length(bundesland) > 1 || !is.na(bundesland)) {
     plzShapes <- plzShapes[plzShapes$id %in% populationData$plz[populationData$bundesland %in% bundesland], ]
@@ -33,14 +40,14 @@ dePlzHeatmap <- function(data, title = "", bundesland = NA,
   plotDf <- plotDf[order(plotDf$order), ]
 
   ggplot2::ggplot() +
-    geom_polygon(data = plotDf,
-                 aes(x = long, y = lat, group = group, fill = .data[[otherColName]])) +
-    scale_colour_gradient(
+    ggplot2::geom_polygon(data = plotDf,
+                 ggplot2::aes(x = long, y = lat, group = group, fill = .data[[otherColName]])) +
+    ggplot2::scale_colour_gradient(
       low = "#FFFFFF", high = color, na.value = "grey50",
       guide = "colourbar", aesthetics = "fill"
     ) +
-    coord_map() +
+    ggplot2::coord_map() +
     ggmap::theme_nothing(legend = TRUE) +
-    ggtitle(title) +
-    theme(plot.title = element_text(hjust = 0.5)) # center title
+    ggplot2::ggtitle(title) +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) # center title
 }
--- tests/testthat/test-dePlzHeatmap.R
+++ tests/testthat/test-dePlzHeatmap.R
@@ -4,4 +4,11 @@ test_that("Plotting works", {
   plt <- dePlzHeatmap(data[, c("plz", "population")], title = "Population in RLP",
                       bundesland = "Rheinland-Pfalz")
   expect_s3_class(plt, "ggplot")
+
+  data2 <- data.frame(plz = data$plz, Zwei = data$population*2)
+  plt <- dePlzHeatmap(data2, title = "Population in RLP",
+                      populationRelative = "pro Einwohner",
+                      bundesland = "Rheinland-Pfalz")
+  expect_s3_class(plt, "ggplot")
+
 })
